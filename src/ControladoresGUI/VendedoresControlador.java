9af06fb5c30d309b6af8d3da7ed5f30b6d7f2797
luis__000 <luis__000@THOR-PC>
2014-08-23 20:57:43 -0600

    Se Agrego el Reporte de Ventas de Vendedores por Periodo

--- src/ControladoresGUI/VendedoresControlador.java
+++ src/ControladoresGUI/VendedoresControlador.java
@@ -11,7 +11,12 @@ import Especiales.Validaciones;
 import GestorDeTablasJPA.ITipoVendedor;
 import GestorDeTablasJPA.IVendedores;
 import Modelos.VendedorModelo;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -21,6 +26,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 
 /**
  * FXML Controller class
@@ -41,9 +47,13 @@ public class VendedoresControlador
     public TextField TFBusqueda;
     public TextField TFAnual;
     public TextField TFAño;
+    public TextField TFFechaI;
+    public TextField TFFechaF;
     public ComboBox ComBoMes;
     private ObservableList<VendedorModelo> data;
     private Validaciones Validar;
+    private int ContadorTeclaI;
+    private int ContadorTeclaF;
     
     public void initialize() {}
     
@@ -226,6 +236,41 @@ public class VendedoresControlador
         }    
     }
     
+    public void AbrirReporteVentasPeriodo(ActionEvent event)
+    {
+        VendedorModelo Seleccionado = (VendedorModelo) TablaVendedores.getSelectionModel().getSelectedItem();
+        if(Seleccionado == null)
+        {
+            this.VendedoresAdministrador.showMensajes("Debe Selecionar un Dato");
+        }
+        else
+        {
+            GeneradordeReportes Reporte = new GeneradordeReportes();
+            if(ValidarFecha(TFFechaI.getText()) && ValidarFecha(TFFechaF.getText()))
+            {
+                String FechaIn = TFFechaI.getText();
+                String FechaFin = TFFechaF.getText();
+                DateFormat Formato = DateFormat.getDateInstance(DateFormat.SHORT);
+                Date FechaI = null;
+                Date FechaF = null;
+                try 
+                {
+                    FechaI = Formato.parse(FechaIn);
+                    FechaF = Formato.parse(FechaFin);
+                } 
+                catch (ParseException ex) 
+                {
+                    Logger.getLogger(NuevoProductoControlador.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                Reporte.AbrirReporte("VentasPorVendedores.jasper",Seleccionado.getId(),FechaI,FechaF);
+            }
+            else
+            {
+                this.VendedoresAdministrador.showMensajes("Verifique los campos Fecha Inicio o Fecha Fin");
+            }  
+        }
+    }
+    
     private int ConvertirMes(String Nombre)
     {
         if("Enero".equals(Nombre))
@@ -279,6 +324,188 @@ public class VendedoresControlador
         else
         {
             return 0;
+        }
+    }
+    
+    public void SoltoTeclaI(KeyEvent e)
+    {
+        String Tecla = e.getText();
+        int Key;
+        Key = e.getCode().impl_getCode();
+        boolean Valido = Validar.ValidarNumero(Tecla);
+        if(Key == 8)
+        {
+            ComprobarTamañoI();
+        }
+        else if(Key == 111 || Key == 55)
+        {
+            AgregarCeroI();
+        }
+        else
+        {
+            if((Key > 46 && Key < 106))
+            {
+                if(!Valido)
+                {
+                    TFFechaI.deletePreviousChar();
+                }
+                else
+                {
+
+                    ContadorTeclaI++;
+                    if(TFFechaI.getText().length()<6)
+                    {
+                        if(ContadorTeclaI == 2)
+                        {
+                            TFFechaI.setText(TFFechaI.getText()+"/");
+                            TFFechaI.end();
+                            ComprobarTamañoI();
+                        }
+                    }
+                    else if(TFFechaI.getText().length() > 10)
+                    {
+                        TFFechaI.deletePreviousChar();
+                    }
+                    ComprobarTamañoI();
+                }
+            }
+        }
+    }
+    
+    private void ComprobarTamañoI()
+    {
+        if(TFFechaI.getText().length() == 0 || TFFechaI.getText().length() == 3 || TFFechaI.getText().length() == 5)
+        {
+            ContadorTeclaI = 0;
+        }
+        if(TFFechaI.getText().length() == 1 || TFFechaI.getText().length() == 4)
+        {
+            ContadorTeclaI = 1;
+        }
+    }
+    
+    private void AgregarCeroI()
+    {
+        if(TFFechaI.getText().length() == 2)
+        {
+            TFFechaI.setText("0"+TFFechaI.getText());
+            TFFechaI.end();
+            ContadorTeclaI = 0;
+        }
+        else if(TFFechaI.getText().length() == 5)
+        {
+            TFFechaI.insertText(3, "0");
+            TFFechaI.end();
+            ContadorTeclaI = 0;
+        }
+        else if(TFFechaI.getText().length() < 3)
+        {
+            TFFechaI.deletePreviousChar();
+        }
+        else if(TFFechaI.getText().length() > 3 && TFFechaI.getText().length() < 6)
+        {
+            TFFechaI.deletePreviousChar();
+        }
+        else if(TFFechaI.getText().length() > 6)
+        {
+            TFFechaI.deletePreviousChar();
+        }
+    }
+    
+    public void SoltoTeclaF(KeyEvent e)
+    {
+        String Tecla = e.getText();
+        int Key;
+        Key = e.getCode().impl_getCode();
+        boolean Valido = Validar.ValidarNumero(Tecla);
+        if(Key == 8)
+        {
+            ComprobarTamañoF();
+        }
+        else if(Key == 111 || Key == 55)
+        {
+            AgregarCeroF();
+        }
+        else
+        {
+            if((Key > 46 && Key < 106))
+            {
+                if(!Valido)
+                {
+                    TFFechaF.deletePreviousChar();
+                }
+                else
+                {
+
+                    ContadorTeclaF++;
+                    if(TFFechaF.getText().length()<6)
+                    {
+                        if(ContadorTeclaF == 2)
+                        {
+                            TFFechaF.setText(TFFechaF.getText()+"/");
+                            TFFechaF.end();
+                            ComprobarTamañoF();
+                        }
+                    }
+                    else if(TFFechaF.getText().length() > 10)
+                    {
+                        TFFechaF.deletePreviousChar();
+                    }
+                    ComprobarTamañoF();
+                }
+            }
+        }
+    }
+    
+    private void ComprobarTamañoF()
+    {
+        if(TFFechaF.getText().length() == 0 || TFFechaF.getText().length() == 3 || TFFechaF.getText().length() == 5)
+        {
+            ContadorTeclaF = 0;
+        }
+        if(TFFechaF.getText().length() == 1 || TFFechaF.getText().length() == 4)
+        {
+            ContadorTeclaF = 1;
+        }
+    }
+    
+    private void AgregarCeroF()
+    {
+        if(TFFechaF.getText().length() == 2)
+        {
+            TFFechaF.setText("0"+TFFechaF.getText());
+            TFFechaF.end();
+            ContadorTeclaF = 0;
+        }
+        else if(TFFechaF.getText().length() == 5)
+        {
+            TFFechaF.insertText(3, "0");
+            TFFechaF.end();
+            ContadorTeclaF = 0;
+        }
+        else if(TFFechaF.getText().length() < 3)
+        {
+            TFFechaF.deletePreviousChar();
+        }
+        else if(TFFechaF.getText().length() > 3 && TFFechaF.getText().length() < 6)
+        {
+            TFFechaF.deletePreviousChar();
+        }
+        else if(TFFechaF.getText().length() > 6)
+        {
+            TFFechaF.deletePreviousChar();
+        }
+    }
+    
+    private boolean ValidarFecha(String Fecha)
+    {
+        if(Validar.FormatoFecha(Fecha))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
     
